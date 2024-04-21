ea971b9eda16f4f335efee78ede6b9e174ffebb6
Louis Pallett <louis.pallett@outlook.com>
2024-04-21 19:01:56 +0100

    Implement client-side form validation

--- client/src/Components/SignIn.jsx
+++ client/src/Components/SignIn.jsx
@@ -1,16 +1,19 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
     return (
         <>
             <form action="#" method="POST" className="space-y-6">
+            <hr />
+            <h2 className="text-xl font-bold text-center m-0 p-0">Login</h2>
+            <hr />
                 <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                    <input type="email" autoComplete="email" required id="email" name="email" className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" />
+                    <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                    <input autoComplete="username" required id="username" name="username" className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" />
                 </div>
                 <div>
                     <div className="flex items-center justify-between"></div>
-                    <label htmlFor="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
                     <input type="password" id="password" name="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"/>
                     <div className="mt-2"></div>
                     <div className="text-sm">
@@ -18,7 +21,7 @@ export default function SignIn() {
                     </div>
                 </div>
                 <div>
-                    <button type="submit" className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Sign In</button>
+                    <button type="submit" className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Login</button>
                 </div>
             </form>
             <p className="mt-10 text-center text-sm text-gray-500"> Not a member? <Link to="/users/sign-up" className="font-semibold leading-6 text-blue-600 hover:text-blue-500">Sign Up</Link></p>
--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,7 +1,82 @@
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+import { useRef } from "react";
+
 export default function SignUp() {
+    // See the playlist on react-hook-form (https://www.youtube.com/playlist?list=PLC3y8-rFHvwjmgBr1327BA5bVXoQH-w5s)
+    const form = useForm();
+    const { register, control, handleSubmit, formState, watch } = form;
+    const { errors } = formState;
+
+    const onSubmit = (data) => {
+        console.log("Form submitted");
+        console.log(data)
+    }
+
     return (
-        <div>
-            <h1>sign up!</h1>
-        </div>
+        <>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6" noValidate>
+                <hr />
+                <h2 className="text-xl font-bold text-center m-0 p-0">Sign Up</h2>
+                <hr />
+                <div>
+                    <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                    <input autoComplete="username" required id="username" {...register("username", {
+                        required: "Username is required",
+                        maxLength: {
+                            value: 20,
+                            message: "Username cannot be longer than twenty (20) characters long!"
+                        },
+                    })}
+                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" />
+                </div>
+                <span className="text-sm font-bold text-red-600">
+                        <p>{errors.username?.message}</p>
+                </span>
+                <div>
+                    <div className="flex items-center justify-between gap-5">
+                        <div>
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                            <input type="password" id="password" autoComplete="current-password" required {...register("password", {
+                                required: "Password is required",
+                                minLength: {
+                                    value: 8,
+                                    message: "Password must be at least eight (8) characters long"
+                                },
+                                pattern: {
+                                    value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                                    message: "Must contain: uppercase, lowercase, number, and special character"
+                                },
+                                validate: {
+                                    passwordMatch: (fieldValue) => {
+                                        return (
+                                            fieldValue == watch("confPassword") || "Passwords do not match"
+                                        )
+                                    }
+                                }
+                            })} 
+                                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"/>
+                        </div>
+                        <div>
+                            <label htmlFor="confPassword" className="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
+                            <input type="password" id="confPassword" required {...register("confPassword", {
+                                required: "Please confirm your password"
+                            })}
+                                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"/>
+                        </div>
+                    </div>
+                    <span className="text-sm font-bold text-red-600">
+                        <p>{errors.password?.message}</p>
+                        <p>{errors.confPassword?.message}</p>
+                    </span>
+                </div>
+                <div>
+                    <button type="submit" className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Sign Up</button>
+                </div>
+            </form>
+            <DevTool control={control}/>
+            <p className="mt-10 text-center text-sm text-gray-500"> Already a member? <Link to="/users/sign-in" className="font-semibold leading-6 text-blue-600 hover:text-blue-500">Login</Link></p>
+        </>
     )
 }
