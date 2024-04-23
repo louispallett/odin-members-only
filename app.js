cbe814af6211d3110e22f0357b25b38a5caa2967
louispallett <louis.pallett@outlook.com>
2024-04-23 23:52:47 +0100

    Configure passport and mongoose to post users on sign in and login

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require("mongoose");
+const session = require("express-session")
+
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require("./routes/users.js");
+const passportLogic = require("./public/javascripts/passport.js")
 
 mongoose.set("strictQuery", false);
-const mongoDB = process.env.MONGODB_URI || connection;
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -24,12 +28,30 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passportLogic.initialize());
+app.use(passportLogic.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.use('/', indexRouter);
 app.use("/users", usersRouter);
+
+app.post("/users/sign-in", passportLogic.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/users/sign-up"
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
 router.get('/sign-up', (req, res, next) => {
   res.render('sign-up', { title: 'Sign Up' });
@@ -7,6 +9,22 @@ router.get('/sign-up', (req, res, next) => {
 
 router.get("/sign-in", (req, res, next) => {
   res.render("sign-in", { title: "Sign In" });
-})
+});
+
+router.post("/sign-up", async (req, res, next) => {
+  try {
+    bcrypt.hash(req.body.password, 10, async (err, hashedpassword) => {
+      const user = new User({
+        username: req.body.username,
+        password: hashedpassword,
+        member: false,
+      });
+      await user.save();
+      res.redirect("/");
+    })
+  } catch(err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
