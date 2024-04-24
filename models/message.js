1316fd16ddb10af68ce004207f80a9e70b991d2d
louispallett <louis.pallett@outlook.com>
2024-04-24 17:05:30 +0100

    Add message model

--- models/message.js
+++ models/message.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    message: { type: String, required: true },
+    subject: { type: String },
+    content: { type: String, required: true },
     date: { type: Date, required: true }
 });
 
