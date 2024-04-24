d8525ad54eebdaed585a88a50016476d5a69bc8c
louispallett <louis.pallett@outlook.com>
2024-04-24 17:06:04 +0100

    Add message list to dashboard

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+
+const Message = require("../models/message");
 const User = require("../models/user");
 
 router.get('/sign-up', (req, res, next) => {
@@ -11,9 +14,14 @@ router.get("/sign-in", (req, res, next) => {
   res.render("sign-in", { title: "Sign In" });
 });
 
-router.get("/dashboard", (req, res, next) => {
-  res.render("dashboard", { title: "Dashboard" });
-});
+router.get("/dashboard", asyncHandler(async (req, res, next) => {
+  const [messages, authors] = await Promise.all([
+    Message.find().sort({ date:-1 }).exec(),
+    User.find().exec()
+  ]);
+
+  res.render("dashboard", { messages: messages, authors: authors });
+}));
 
 router.post("/sign-up", async (req, res, next) => {
   try {
@@ -24,7 +32,7 @@ router.post("/sign-up", async (req, res, next) => {
         member: false,
       });
       await user.save();
-      res.redirect("/dashboard");
+      res.redirect("/users/sign-in");
     })
   } catch(err) {
     return next(err);
