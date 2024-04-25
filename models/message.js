d7e81ee9af48d712a79acc2e39c35420092588fc
Louis Pallett <louis.pallett@outlook.com>
2024-04-25 22:03:35 +0100

    Format date in messages

--- models/message.js
+++ models/message.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
@@ -7,5 +8,10 @@ const MessageSchema = new Schema({
     content: { type: String, required: true },
     date: { type: Date, required: true }
 });
+
+
+MessageSchema.virtual("date_formatted").get(function () {
+    return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED);
+})
 
 module.exports = mongoose.model("Message", MessageSchema);
