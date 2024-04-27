3b5fbaf058179a923f421cd0a26d15b051d23f6c
louispallett <louis.pallett@outlook.com>
2024-04-27 09:48:11 +0100

    Add char counter to subject line

--- public/javascripts/dom-content.js
+++ public/javascripts/dom-content.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const content = document.getElementById("content");
-    const counter = document.getElementById("counter");
+    const counter = document.getElementById("msg-counter");
+    const maxLength = content.getAttribute("maxLength");
+
+    counter.innerHTML = "0/" + maxLength;
+    content.addEventListener("input", (e) => {
+        const target = e.target;
+        const currentLength = target.value.length;
+        counter.innerHTML = currentLength + "/" + maxLength;
+    });
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    const content = document.getElementById("subject");
+    const counter = document.getElementById("sub-counter");
     const maxLength = content.getAttribute("maxLength");
 
     counter.innerHTML = "0/" + maxLength;
