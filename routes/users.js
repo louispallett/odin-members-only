485a0ab4bf3fe53776f32652718ccfec83fb8988
louispallett <louis.pallett@outlook.com>
2024-04-28 23:41:09 +0100

    Add sanitation and validation

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 const Message = require("../models/message");
 const User = require("../models/user");
 
-router.get('/sign-up', (req, res, next) => {
-  res.render('sign-up', { title: 'Sign Up' });
+router.get("/sign-up", (req, res, next) => {
+  res.render("sign-up", { title: "Sign Up" });
 });
 
 router.get("/sign-in", (req, res, next) => {
@@ -17,31 +18,65 @@ router.get("/sign-in", (req, res, next) => {
 router.get("/dashboard", asyncHandler(async (req, res, next) => {
   if (req.isAuthenticated()) {
     const messages = await Message.find().populate("author").sort({ date:-1 }).exec();
-  
     res.render("dashboard", { messages: messages, user: req.user });
   } else {
     res.redirect("/");
   }
 }));
 
-router.post("/sign-up", async (req, res, next) => {
-  try {
-    bcrypt.hash(req.body.password, 10, async (err, hashedpassword) => {
-      const user = new User({
-        username: req.body.username,
-        password: hashedpassword,
-      });
-      await user.save();
-      res.redirect("/users/sign-in");
-    })
-  } catch(err) {
-    return next(err);
-  }
-});
+router.post("/sign-up", 
+  body("username", "Username must be between 3 and 15 characters")
+    .trim()
+    .isLength({ min:3, max:15 })
+    .escape(),
+  body("password")
+    .trim()
+    .escape(),
+  body("confPassword")
+    .trim()
+    .escape(),
 
-router.post("/dashboard/members", asyncHandler(async (req, res, next) => {
-  //TODO: Add sanitation
-  
+  asyncHandler (async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("sign-up", { 
+        title: "Sign Up",
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    const username_exists = await User.findOne({ username: req.body.username });
+    if (username_exists) {
+      res.render("sign-up", { 
+        title: "Sign Up",
+        errors: [{ msg: "Username taken!" }],
+      });
+      return;
+    }
+
+    try {
+      bcrypt.hash(req.body.password, 10, async (err, hashedpassword) => {
+        const user = new User({
+          username: req.body.username,
+          password: hashedpassword,
+        });
+        await user.save();
+        res.redirect("/users/sign-in");
+      })
+    } catch(err) {
+      return next(err);
+    }
+  })
+);
+
+router.post("/dashboard/members", 
+  body("memberPass", "Please complete this field")
+    .trim()
+    .isLength({ min:1 })
+    .escape(),
+
+asyncHandler(async (req, res, next) => {
   if (req.body.memberPass == process.env.MEMBER_PASS) {
     const user = new User({
       username: req.user.username,
@@ -50,49 +85,67 @@ router.post("/dashboard/members", asyncHandler(async (req, res, next) => {
       admin: false,
       _id: req.user._id
     });
-
     await User.findByIdAndUpdate(req.user._id, user);
   }
   res.redirect("/users/dashboard")
 }));
 
-router.post("/dashboard/admin", asyncHandler(async (req, res, next) => {
-  //TODO: Add sanitation
+router.post("/dashboard/admin",
+  body("adminPass", "Please complete this field")
+    .trim()
+    .isLength({ min:1 })
+    .escape(),
 
-  if (req.body.adminPass == process.env.ADMIN_PASS) {
-    const user = new User({
-      username: req.user.username,
-      password: req.user.password,
-      member: req.user.member,
-      admin: true,
-      _id: req.user._id
-    });
+  asyncHandler(async (req, res, next) => {
+    if (req.body.adminPass == process.env.ADMIN_PASS) {
+      const user = new User({
+        username: req.user.username,
+        password: req.user.password,
+        member: req.user.member,
+        admin: true,
+        _id: req.user._id
+      });
 
-    await User.findByIdAndUpdate(req.user._id, user);
-  }
-  res.redirect("/users/dashboard")
-}));
+      await User.findByIdAndUpdate(req.user._id, user);
+    }
+    res.redirect("/users/dashboard")
+  })
+);
+
 
 router.post("/dashboard/delete-msg", asyncHandler(async (req, res, next) => {
   await Message.findByIdAndDelete(req.body.delMsgId);
   res.redirect("/");
 }));
 
-router.post("/dashboard/new-message", asyncHandler(async (req, res, next) => {
-  //TODO: sanatize form here
+router.post("/dashboard/new-message", 
+  body("subject", "")
+    .trim()
+    .isLength({ min:1, max:30 })
+    .escape(),
+  body("content", "")
+    .trim()
+    .isLength({ min:1, max:300 })
+    .escape(),
+  
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.redirect("/");
+      return;
+    }
 
-  const message = new Message({
-    author: req.user._id,
-    subject: req.body.subject,
-    content: req.body.content,
-    date: new Date()    
-  });
+    const message = new Message({
+      author: req.user._id,
+      subject: req.body.subject,
+      content: req.body.content,
+      date: new Date()    
+    });
 
-  //TODO: Check for errors in sanitation
-
-  await message.save();
-  res.redirect("/users/dashboard")
-}));
+    await message.save();
+    res.redirect("/users/dashboard")
+  })
+);
 
 router.get("/logout", (req, res, next) => {
   // Note that req.logout() (passportJS) now requires a callback - something like this:
