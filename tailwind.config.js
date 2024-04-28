e162748948b6f80b6ecdfeced5409b00c7a07f80
louispallett <louis.pallett@outlook.com>
2024-04-28 19:16:15 +0100

    Update/Add media queries (responsive ui)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./views/*.pug",
-      "./public/javascripts/*.js",
-    ],
-    theme: {
-      extend: {
-        gridTemplateColumns: {
-          "dashboard": "2fr 1fr",
-          "message": "50px 4fr 1fr",
-        },
-        spacing: {
-          "screen": "1980px",
-        }
+  content: [
+    "./views/*.pug",
+    "./public/javascripts/*.js",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "dashboard": "2fr 1fr",
+        "message": "50px 4fr 1fr",
       },
+      spacing: {
+        "screen": "1980px",
+      }
     },
-    plugins: [],
+  },
+  plugins: [],
 }
